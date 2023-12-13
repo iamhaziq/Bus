2091d2cfc7715fee7b4b12a629f0457c290d9c23
rafiqi12 <139891444+rafiqi12@users.noreply.github.com>
2023-12-13 10:05:43 +0800

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+ublic class BusTicketReservationSystem {
+    public static void main(String[] args) {
+        // Create an instance of the Bus class
+        Bus bus = new Bus(30);
+
+        // Create an instance of the Reservation class
+        Reservation reservation = new Reservation(bus, "John Doe");
+
+        // Example of reserving seats
+        reservation.reserveSeat(1);
+        reservation.reserveSeat(2);
+        reservation.reserveSeat(3);
+
+        // Display available seats after reservations
+        bus.displayAvailableSeats();
+    }
+}
